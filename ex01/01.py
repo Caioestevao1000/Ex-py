9983bf71f9cc29699a6b0f7521bbde6f12363031
caioestevao1000 <caioestevao1000@gmail.com>
2025-04-14 18:06:08 -0300

    exercício py, multiplicação de 3 valores

---
+++ ex01/01.py
@@ -0,0 +1,6 @@
+print("## Escreva 3 números para multiplicar ##")
+
+n1 = float(input("Digite um número:"))
+n2 = float(input("Digite um número:"))
+n3 = float(input("Digite um número:"))
+print("O resultado dessa multiplicação será de:", n1*n2*n3)
