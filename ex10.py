cfe03678065532c67e9c994dd9cbc00027a509d6
Caio Estevão <108750017+Caioestevao1000@users.noreply.github.com>
2025-04-15 22:55:38 -0300

    subindo exercicio 10 ao 14

--- ex10.py
+++ ex10.py
@@ -1,1 +1,10 @@
 print("## Quantidade de Salário Recebidos ##")
+
+salariomin = float(input("Digite o Salário Mínimo:"))
+salario = float(input("Digite o seu Salário:"))
+
+qntsalario = salario / salariomin
+
+print("\nVocê ganha:",qntsalario,"salário mínimos")
+
+input("\nPressione Enter para sair...")
---
+++ ex12.py
@@ -0,0 +1,13 @@
+print("## Cálculo da idade ##")
+
+idade = int(input("Digite seu ano de nascimento:"))
+ano_atual = int(input("Digite o ano atual:"))
+
+ano = ano_atual - idade
+
+print("\nSua idade é igual", ano)
+print("\nSua idade em meses é igual:", ano * 12)
+print("\nSua idade em dias é igual:", ano * 365 , "considerado que todo os anos tem 365 dias")
+print("\nSua idade em semans é igual:", ano * 52.18)
+
+input("\nPressione Enter para sair...")
