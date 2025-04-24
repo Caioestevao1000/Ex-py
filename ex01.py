2e08db3f5dcce67517850da07304538f2b26a6bb
caioestevao1000 <caioestevao1000@gmail.com>
2025-04-24 16:23:30 -0300

    Excluindo execícios, e deixando separado nas pastas

--- ex01.py
+++
@@ -1,9 +0,0 @@
-print("## Escreva 3 números para multiplicar ##")
-
-n1 = float(input("Digite o 1°número:"))
-n2 = float(input("Digite o 2°número:"))
-n3 = float(input("Digite o 3°número:"))
-
-print("O resultado dessa multiplicação será de:", n1*n2*n3)
-
-input("\nPressione Enter para sair...")
