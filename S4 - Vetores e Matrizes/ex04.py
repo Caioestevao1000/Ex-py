170ec87116739330d9d3b7198c0e9f9dfec314c9
caioestevao1000 <caioestevao1000@gmail.com>
2025-06-30 23:35:53 -0300

    outra forma de fazer

--- S4 - Vetores e Matrizes/ex04.py
+++ S4 - Vetores e Matrizes/ex04.py
@@ -35,3 +35,29 @@ else:
         print("Não há Aluno Cursando as Duas Máterias")
     else:    
         print(f"Essas são as matrículas dos Alunos que Cursam os Dois Cursos {comum}")
+
+# print("\n## Alunos que Cursam as Duas Máterias ##")
+
+# lp = [] 
+# lc = [] 
+# comum = []
+
+# print ("Preencha com os RAs do alunos de Linguagem de Programação...")
+# for p in range(3):
+#     ra = int(input(f"Digite o RA do {p+1}° Aluno:"))
+#     lp.append(ra)
+# else:
+#     print ("Preencha com os RAs do alunos de Lógica Computacional...")
+#     for c in range(2):
+#         ra = int(input(f"Digite o RA do {c+1}° Aluno:"))
+#         lc.append(ra)
+
+# for x in range(len(lc)):
+#     for j in range(len(lp)):
+#         if lc[x] == lp[j]:
+#             comum.append(lc[x])
+#             break
+# if len(comum) == 0:
+#     print("Não há alunos cursando as duas máterias")
+# else: 
+#     print(f"Esses são os RAs dos alunos que cursam os dois cursos {comum}")
